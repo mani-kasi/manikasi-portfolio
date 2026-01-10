286df7c03822bc711831ea0382f796ffdcc8e4dc
Mani Kasi <111610740+mani-kasi@users.noreply.github.com>
2026-01-09 19:23:59 -0500

    edit path

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,7 +13,7 @@ export const Hero = () => {
         </p>
         <div className={styles.buttonGroup}>
           <a
-            href="/public/resume.pdf"
+            href="/resume.pdf"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.contactBtn}
